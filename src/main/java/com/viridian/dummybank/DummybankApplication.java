4cb7c76cf144f5a6fc59e5b7367443c7a4d2175c
Isabraxas <isvar.vega@gmail.com>
2018-02-20 09:40:20 -0400

    incio proyecto VDB

---
+++ src/main/java/com/viridian/dummybank/DummybankApplication.java
@@ -0,0 +1,12 @@
+package com.viridian.dummybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummybankApplication.class, args);
+	}
+}
